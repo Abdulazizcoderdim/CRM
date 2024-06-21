59f7a620dd94816edfaff001185907edad5e22e7
AbdulazizRus <coderdim@gmail.com>
2024-06-21 09:53:51 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,65 @@
-import { 
-    Refine,
-    GitHubBanner, 
-    WelcomePage,
-    Authenticated, 
-} from '@refinedev/core';
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import {
+  Refine,
+  GitHubBanner,
+  WelcomePage,
+  Authenticated,
+} from '@refinedev/core'
+import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools'
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
 
-import { AuthPage,ErrorComponent
-,useNotificationProvider
-,ThemedLayoutV2
-,ThemedSiderV2} from '@refinedev/antd';
-import "@refinedev/antd/dist/reset.css";
+import {
+  AuthPage,
+  ErrorComponent,
+  useNotificationProvider,
+  ThemedLayoutV2,
+  ThemedSiderV2,
+} from '@refinedev/antd'
+import '@refinedev/antd/dist/reset.css'
 
-import dataProvider, { GraphQLClient, liveProvider } from "@refinedev/nestjs-query";
-import { createClient } from "graphql-ws";
-import { App as AntdApp } from "antd"
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { Header } from "./components/header";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import { ForgotPassword } from "./pages/forgotPassword";
-import { authProvider } from "./authProvider";
+import { dataProvider, liveProvider } from './providers'
 
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
-
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
-
-
-
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from '@refinedev/react-router-v6'
+import { App as AntdApp } from 'antd'
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom'
 
 function App() {
-    
-
-    
-    
-    return (
-        <BrowserRouter>
-        <GitHubBanner />
-        <RefineKbarProvider>
-            <ColorModeContextProvider>
-<AntdApp>
-            <DevtoolsProvider>
-                <Refine dataProvider={dataProvider(gqlClient)}
-liveProvider={liveProvider(wsClient)}
-notificationProvider={useNotificationProvider}
-routerProvider={routerBindings}
-authProvider={authProvider} 
-                    options={{
-                        syncWithLocation: true,
-                        warnWhenUnsavedChanges: true,
-                        useNewQueryKeys: true,
-                            projectId: "nrmoBZ-0Jrs0a-RV17JI",
-                        liveMode: "auto",
-                    }}
-                >
-
-
-                        <Routes>
-                            <Route index element={<WelcomePage />} />
-                        </Routes>
-                    <RefineKbar />
-                    <UnsavedChangesNotifier />
-                    <DocumentTitleHandler />
-                </Refine>
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
+              notificationProvider={useNotificationProvider}
+              routerProvider={routerBindings}
+              // authProvider={}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                useNewQueryKeys: true,
+                projectId: 'nrmoBZ-0Jrs0a-RV17JI',
+                liveMode: 'auto',
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
             <DevtoolsPanel />
-            </DevtoolsProvider>
-            </AntdApp>
-</ColorModeContextProvider>
-        </RefineKbarProvider>
-        </BrowserRouter>
-      );
-};
+          </DevtoolsProvider>
+        </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  )
+}
 
-export default App;
+export default App
+//11:00
